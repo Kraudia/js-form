8f0526460eb58ab367d795020abc0c980911f583
Kraudia <klaudienne@gmail.com>
2017-05-17 23:27:47 +0200

    arrows

--- js/script.js
+++ js/script.js
@@ -28,10 +28,27 @@ var ids = [
 
 $.fn.start = function() {
     var ul = $(this);
-    var error = "<span class='error'>Wybierz miesiąc</span>";
+    var error = '<span class="error">Wybierz miesiąc</span>';
+
+    //arrows
+    var arrowDown =
+        '<img src="./assets/down-dark.png" align="right" style="height: 20px;"/>';
+
+    var arrowUp =
+        '<img src="./assets/up-dark.png" align="right" style="height: 20px;"/>';
+
 
     if (ul.hasClass("select__ul--error")) {
         ul.parent().append(error);
+    }
+
+    ul.find("li").find('img').remove();
+
+    if (ul.hasClass("select__ul--selected")) {
+        var selectedLi = $("li.selected");
+        ul.find(selectedLi).append(arrowDown);
+    } else {
+        ul.find("li:first-of-type").append(arrowDown);
     }
 
     $(document).click(function(event) {
@@ -41,6 +58,15 @@ $.fn.start = function() {
             var selectedLi = $("li.selected");
             if (ul.find(selectedLi).text()=="Miesiąc") {
                 ul.removeClass("select__ul--selected").addClass("select__ul select__ul--error");
+            }
+
+
+            ul.find("li").find('img').remove();
+            if (ul.hasClass("select__ul--selected")) {
+                var selectedLi = $("li.selected");
+                ul.find(selectedLi).append(arrowDown);
+            } else {
+                ul.find("li:first-of-type").append(arrowDown);
             }
         }
     });
@@ -54,17 +80,25 @@ $.fn.start = function() {
         }
 
         if (ul.hasClass("select__ul--active")) {
+            ul.find("li").find('img').remove();
+
             var selectedLi = $("li.selected");
             ul.find(selectedLi).removeClass("selected");
-
-            li.addClass("selected");
+            ul.find(selectedLi).find('img').remove();
 
             if (ul.hasClass("select__ul")) {
                 ul.removeClass("select__ul").addClass("select__ul--selected");
             }
 
             ul.removeClass("select__ul--active");
+            li.addClass("selected").append(arrowDown);
         } else {
+            ul.find("li").find('img').remove();
+
+            ul.children("li:nth-child(2)").append(arrowUp);
+
+            ul.find(selectedLi).removeClass("selected");
+            ul.find(selectedLi).find('img').remove();
             ul.addClass("select__ul--active");
         }
 
@@ -78,6 +112,8 @@ $.fn.start = function() {
     });
 };
 
+$("#wylaczone").start();
+$("#wylaczonewypelnione").start();
 $("#puste").start();
 $("#wybrane").start();
 $("#bledne").start();
