9a2954886d88c3ff9d145bafbfa25a0c021769b6
Kraudia <klaudienne@gmail.com>
2017-05-17 22:11:26 +0200

    error select works

--- js/script.js
+++ js/script.js
@@ -31,20 +31,23 @@ $.fn.start = function() {
     $(this).on("click", "li", function(event) {
         var li = $(this);
 
-
+        if (ul.hasClass("select__ul--error")) {
+            ul.removeClass("select__ul--error");
+        }
 
         if (ul.hasClass("select__ul--active")) {
             var selectedLi = $("li.selected");
             ul.find(selectedLi).removeClass("selected");
 
             li.addClass("selected");
+
+            if (ul.hasClass("select__ul")) {
+                ul.removeClass("select__ul").addClass("select__ul--selected");
+            }
+
             ul.removeClass("select__ul--active");
         } else {
             ul.addClass("select__ul--active");
-
-            if(ul.hasClass("select__ul--error")) {
-                ul.removeClass("select__ul--error");
-            }
         }
 
         if (ul.hasClass("select__ul--selected")) {
