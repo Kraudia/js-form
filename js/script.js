c8bcbdcb8298df79c9786eb88540dc6528a1229d
Kraudia <klaudienne@gmail.com>
2017-05-17 22:39:20 +0200

    message error

--- js/script.js
+++ js/script.js
@@ -24,15 +24,33 @@ var ids = [
 ];
 
 
+
+
 $.fn.start = function() {
     var ul = $(this);
+    var error = "<span class='error'>Wybierz miesiąc</span>";
 
-    var ul2 = $(this);
+    if (ul.hasClass("select__ul--error")) {
+        ul.parent().append(error);
+    }
+
+    $(document).click(function(event) {
+        if(!$(event.target).closest(ul).length) {
+            ul.removeClass("select__ul--active");
+
+            var selectedLi = $("li.selected");
+            if (ul.find(selectedLi).text()=="Miesiąc") {
+                ul.removeClass("select__ul--selected").addClass("select__ul select__ul--error");
+            }
+        }
+    });
+
     $(this).on("click", "li", function(event) {
         var li = $(this);
 
         if (ul.hasClass("select__ul--error")) {
             ul.removeClass("select__ul--error");
+            ul.siblings(".error").remove();
         }
 
         if (ul.hasClass("select__ul--active")) {
@@ -54,11 +72,15 @@ $.fn.start = function() {
             var selectedLi = $("li.selected");
             if (ul.find(selectedLi).text()=="Miesiąc") {
                 ul.removeClass("select__ul--selected").addClass("select__ul select__ul--error");
-            } else {
-                console.log("nieprawda, bo ", ul.find(selectedLi).text());
+                ul.parent().append(error);
             }
         }
     });
 };
 
+$("#puste").start();
+$("#wybrane").start();
+$("#bledne").start();
+$("#hover").start();
 $("#aktywne").start();
+
